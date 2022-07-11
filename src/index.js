960eb532c411e6412fad7d5828b75e25773e7344
hilusjr <pietrzef@gmail.com>
2022-07-11 19:51:16 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
@@ -10,7 +10,7 @@ import Gallery from './components/Gallery'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route path="/" element={<App />}>
         <Route index element={<Gallery />} />
@@ -18,6 +18,6 @@ root.render(
         <Route path="contact" element={<Contact />} />
       </Route>
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 )
 reportWebVitals()
